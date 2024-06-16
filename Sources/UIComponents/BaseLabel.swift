287bda48a89afd06cb7ab6d5cd9a6d17e8c85152
yeomsungpil <present7256@gmail.com>
2024-06-16 17:48:21 +0900

    - BaseLabel 추가

---
+++ Sources/UIComponents/BaseLabel.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by 염성필 on 6/16/24.
+//
+
+import Foundation
+import UIKit
+
+open class BaseLabel: UILabel {
+    public init() {
+        super.init(frame: .zero)
+        self.text = "ExampleLabel"
+    }
+    
+    public required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
